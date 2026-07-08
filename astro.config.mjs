62b5d05a1272dd8f079b6378ac5a48159e41ed5d
DynamiKun <1135011463@qq.com>
2026-07-08 18:47:51 +0800

    Configure custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://DKKolmo.github.io',
+  site: 'https://www.kkolmo.com',
   base: '/my-knowledge-base',
   integrations: [
     starlight({
-      title: '我的知识库',
+      title: "Kolmo's Blog",
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/DKKolmo/my-knowledge-base' }],
       sidebar: [
         {
